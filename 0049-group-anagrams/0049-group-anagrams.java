9fcc64fc4a71fe7fe089f2da3678948c77feb3d6
Francesco Galazzo <72932124+FrancescoGalazzo@users.noreply.github.com>
2026-05-24 08:33:02 -0500

    Time: 7 ms (73.71%), Space: 50.3 MB (16.12%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -6,10 +6,17 @@ class Solution {
         for(String s: strs){
             int[] count = new int[26];
             
-            for(char c: s.toCharArray())
-                count[c-'a']++;
-            
-            String key = Arrays.toString(count);
+             for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - 'a']++;
+            }
+
+            // Create a short, fast-to-hash custom string key
+            char[] keyChars = new char[26];
+            for (int i = 0; i < 26; i++) {
+                keyChars[i] = (char) (count[i]);
+            }
+            String key = new String(keyChars);
+
             res.putIfAbsent(key, new ArrayList<>());
             res.get(key).add(s);
         }
