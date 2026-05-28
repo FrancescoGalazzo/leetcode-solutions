8989373b6e3ff95eaa01b37706f9e417355d1822
Francesco Galazzo <72932124+FrancescoGalazzo@users.noreply.github.com>
2026-05-28 13:58:47 -0500

    Time: 148 ms (5.08%), Space: 54.3 MB (52.05%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,6 +1,50 @@
 class Solution {
+
+    private int dist2(int[] p) {
+        return p[0] * p[0] + p[1] * p[1];
+    }
+
+    private int partition(int[][] points, int l, int r) {
+        int[] pivot = points[r];
+        int pivotDist = dist2(pivot);
+        int i = l;
+        for (int j = l; j < r; j++) {
+            if (dist2(points[j]) <= pivotDist) {
+                swap(points, i, j);
+                i++;
+            }
+        }
+        swap(points, i, r);
+        return i;
+    }
+
+    private void quickselect(int[][] points, int l, int r, int K) {
+        if (l >= r) return;
+        int p = partition(points, l, r);
+        int leftCount = p - l + 1;
+        if (leftCount == K) {
+            return;
+        } else if (leftCount > K) {
+            quickselect(points, l, p - 1, K);
+        } else {
+            quickselect(points, p + 1, r, K - leftCount);
+        }
+    }
+
+    private void swap(int[][] points, int i, int j) {
+        int[] tmp = points[i];
+        points[i] = points[j];
+        points[j] = tmp;
+    }
+
+    public int[][] kClosest(int[][] points, int K) {
+        quickselect(points, 0, points.length - 1, K);
+        return Arrays.copyOfRange(points, 0, K);
+    }
+
+    /*
     public int[][] kClosest(int[][] points, int k) {
-                // Keep an int[] where index 0 is squared distance, index 1 is the point's index
+        // Keep an int[] where index 0 is squared distance, index 1 is the point's index
         PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b[0], a[0]));
         int[][] results = new int[k][2];
 
@@ -21,4 +65,5 @@ class Solution {
 
         return results;
     }
+    */
 }
