c0e31af24a7b5f5b18604c1540c3b1e55fc10e03
Francesco Galazzo <72932124+FrancescoGalazzo@users.noreply.github.com>
2026-08-01 16:40:42 -0500

    Time: 9 ms (31.36%), Space: 47.7 MB (19.42%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,40 +1,93 @@
+import java.util.*;
+
 class Solution {
-    private int m, n;
-    private int[][] heights;
-    private int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+    private final int[][] DIRS = {
+        {-1, 0},
+        {1, 0},
+        {0, -1},
+        {0, 1}
+    };
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        this.heights = heights;
-        m = heights.length;
-        n = heights[0].length;
+        int m = heights.length;
+        int n = heights[0].length;
+
         boolean[][] pacific = new boolean[m][n];
         boolean[][] atlantic = new boolean[m][n];
 
+        Queue<int[]> pacificQueue = new ArrayDeque<>();
+        Queue<int[]> atlanticQueue = new ArrayDeque<>();
+
         for (int c = 0; c < n; c++) {
-            dfs(0, c, pacific, Integer.MIN_VALUE);
-            dfs(m-1, c, atlantic, Integer.MIN_VALUE);
-        }
-        for (int r = 0; r < m; r++) {
-            dfs(r, 0, pacific, Integer.MIN_VALUE);
-            dfs(r, n-1, atlantic, Integer.MIN_VALUE);
+            add(0, c, pacific, pacificQueue);
+            add(m - 1, c, atlantic, atlanticQueue);
         }
 
+        for (int r = 0; r < m; r++) {
+            add(r, 0, pacific, pacificQueue);
+            add(r, n - 1, atlantic, atlanticQueue);
+        }
+
+        bfs(heights, pacificQueue, pacific);
+        bfs(heights, atlanticQueue, atlantic);
+
         List<List<Integer>> result = new ArrayList<>();
-        for (int r = 0; r < m; r++)
-            for (int c = 0; c < n; c++)
-                if (pacific[r][c] && atlantic[r][c])
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (pacific[r][c] && atlantic[r][c]) {
                     result.add(Arrays.asList(r, c));
+                }
+            }
+        }
+
         return result;
     }
 
-    private void dfs(int r, int c, boolean[][] visited, int prevHeight) {
-        if (r < 0 || r >= m || c < 0 || c >= n) return;
-        if (visited[r][c]) return;
-        if (heights[r][c] < prevHeight) return;
+    private void add(
+            int r,
+            int c,
+            boolean[][] visited,
+            Queue<int[]> queue
+    ) {
+        if (!visited[r][c]) {
+            visited[r][c] = true;
+            queue.offer(new int[]{r, c});
+        }
+    }
 
-        visited[r][c] = true;
-        for (int[] d : dirs) {
-            dfs(r + d[0], c + d[1], visited, heights[r][c]);
+    private void bfs(
+            int[][] heights,
+            Queue<int[]> queue,
+            boolean[][] visited
+    ) {
+        int m = heights.length;
+        int n = heights[0].length;
+
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int r = cell[0];
+            int c = cell[1];
+
+            for (int[] dir : DIRS) {
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+
+                if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
+                    continue;
+                }
+
+                if (visited[nr][nc]) {
+                    continue;
+                }
+
+                if (heights[nr][nc] < heights[r][c]) {
+                    continue;
+                }
+
+                visited[nr][nc] = true;
+                queue.offer(new int[]{nr, nc});
+            }
         }
     }
 }
